f82f3e3eb9e45e56736691d79f608f00c1e9c5b3
Robertl231 <rloughran67@gmail.com>
2021-12-10 21:04:30 +0000

    moves script src to bottom of html files and made it so when user click 'how to play' the div pops up

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,9 @@
+// How To Play pop-up
+
 var howToPlay = document.getElementById("htp");
+var htpNav = document.getElementById("htp-nav");
+var close = document.getElementsByClassName=("btn-close")
+
+htpNav.onclick = function() {
+    howToPlay.style.display= "block";
+}
