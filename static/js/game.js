453706a06f26c0fc38d17628c1cc2ced07c0c256
Robertl231 <rloughran67@gmail.com>
2021-12-10 23:35:24 +0000

    created the create water pipe function, added a new water pipe div using JS

--- static/js/game.js
+++ static/js/game.js
@@ -6,6 +6,8 @@ let gravity = 3
 let dragonHeight = 450
 let dragonLeft = 200
 
+/*------------------------------------------- start game function */
+
 function startGame() {
     dragon.style.bottom = dragonHeight + 'px'
     dragon.style.left = dragonLeft + "px"
@@ -13,8 +15,19 @@ function startGame() {
 }
 setInterval(startGame, 10)
 
+ /*------------------------------------------- flap function */
+
 function flap() {
     if (dragonHeight < 700) dragonHeight += 150
     dragon.style.bottom = dragonHeight + "px"
     console.log(dragonHeight)
 }
+
+/*------------------------------------------- create water pipe function */
+
+function createWaterPipe() {
+    const WaterPipe = document.createElement('div')
+    WaterPipe.classList.add("water-pipe")
+    gameContainer.appendChild(WaterPipe)
+}
+createWaterPipe()
