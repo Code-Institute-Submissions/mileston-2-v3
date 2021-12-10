2ecd5e614a7c4b53fe212d9c27cd0401d42658c7
Robertl231 <rloughran67@gmail.com>
2021-12-10 23:00:29 +0000

    created start game function and added gravity to the game

--- static/js/game.js
+++ static/js/game.js
@@ -1,3 +1,14 @@
 const gameContainer = document.querySelector(".game-container")
 const ground = document.querySelector(".ground")
 const dragon = document.querySelector(".dragon")
+
+let gravity = 3
+let dragonHeight = 450
+let dragonLeft = 200
+
+function startGame() {
+    dragon.style.bottom = dragonHeight + 'px'
+    dragon.style.left = dragonLeft + "px"
+    dragonHeight -= gravity
+}
+setInterval(startGame, 10)
